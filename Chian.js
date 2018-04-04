595b73fd60f7f17a273f9a4237bd6b48f496402d
ranahiren <ranahiren27@gmail.com>
2018-04-05 01:08:05 +0530

    Final ranaCoin/ming algo changed/ data--->transecction/Transaction class added

--- Chian.js
+++ Chian.js
@@ -1,16 +1,25 @@
 const SHA256 = require('crypto-js/sha256');
+class Transaction{
+
+	constructor(fromAddress, toAddress, amount){
+		this.fromAddress = fromAddress;
+		this.toAddress = toAddress;
+		this.amount = amount;
+	}
+
+}
 class Block{
-	constructor(index, timestamp, data, previousHash = ''){
-		this.index = index;
+
+	constructor(timestamp, transactions, previousHash = ''){
 		this.timestamp = timestamp;
-		this.data = data;
+		this.transactions = transactions;
 		this.previousHash = previousHash;
 		this.hash = this.calculateHash();
 		this.nonce = 0;
 	}
 
 	calculateHash(){
-		return SHA256(this.nonce + this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+		return SHA256(this.nonce + this.previousHash + this.timestamp + JSON.stringify(this.transaction)).toString();
 	} 
 
 	mineBlock(difficulty){
@@ -28,23 +37,55 @@ class Block{
 class Bloackchain{
 	constructor(){
 		this.chain = [this.createGenesisBlock()];
-		this.difficulty = 5;
+		this.difficulty = 3;
+		this.paddingTransactions = [];
+		this.miningReward = 100; 
 	}
 
 	createGenesisBlock(){
-		return new Block(0, "01/01/2018", "Genesis Block", "0");
+		return new Block("01/01/2018", "Genesis Block", "0");
 	}
 	
 	getLatestBlock(){
 		return this.chain[this.chain.length -1 ];
 	}
 
-	addBlock(newBlock){
+	minePendingTransactions(miningRewardAddress){
+		let block = new Block(Date.now(), this.paddingTransactions);
+		block.mineBlock(this.difficulty);
+		console.log("Block successfully mined!!");
+		this.chain.push(block);
+
+		this.paddingTransactions = [
+			new Transaction(null, miningRewardAddress, this.miningReward)
+		];
+	}
+/*	addBlock(newBlock){
 		newBlock.previousHash = this.getLatestBlock().hash;
 		newBlock.mineBlock(this.difficulty);
 		this.chain.push(newBlock);
 	}
+*/	createTransaction(transaction){
+		this.paddingTransactions.push(transaction);
+	}
 
+	getBalanceOfAddress(address){
+		let balance = 0;
+ 
+		for(const block of this.chain){
+			for(const trans of block.transactions){
+				if(trans.fromAddress === address){
+					balance -= trans.amount;
+				}
+
+				if(trans.toAddress === address){
+					balance += trans.amount; 
+				}
+			}
+		}
+
+		return balance; 
+	}
 	isChainValid(){
 		for(let i = 1; i<this.chain.length;  i++){
 			const currentBlock = this.chain[i];
@@ -52,7 +93,7 @@ class Bloackchain{
 			const previousBlock = this.chain[i-1];
 			if(currentBlock.hash !== currentBlock.calculateHash()){
 				console.log("Block "+i+" and "+(i-1)+"has problem!!");
-				console.log("data is changed in "+i+"Block!!");
+				console.log("transaction is changed in "+i+"Block!!");
 				return false;
 			}
 
@@ -68,16 +109,21 @@ class Bloackchain{
 
 }
 
-
+// test code!!!
 let ranaCoin = new Bloackchain();
-console.log("Mining block 1....");
-ranaCoin.addBlock(new Block(1,"11/01/2018",{amount: 23}));
-console.log("Mining block 2....");
-ranaCoin.addBlock(new Block(2,"12/01/2018",{amount: 243}));
-console.log("Mining block 3....");
-ranaCoin.addBlock(new Block(3,"13/01/2018",{amount: 3}));
-console.log("Mining block 4....");
-ranaCoin.addBlock(new Block(4,"14/01/2018",{amount: 2}));
-console.log("Mining block 5....");
-ranaCoin.addBlock(new Block(5,"15/01/2018",{amount: 230}));
+ranaCoin.createTransaction(new Transaction('address1','address2',100));
+ranaCoin.createTransaction(new Transaction('address2','address3',50));
+ranaCoin.createTransaction(new Transaction('address3','address1',25));
 
+
+console.log("\nString the miner....");
+ranaCoin.minePendingTransactions('ranahiren27');
+
+console.log('\n balance of ranahire27 is ',ranaCoin.getBalanceOfAddress('ranahiren27'));
+
+console.log("\nString the miner again....");
+ranaCoin.minePendingTransactions('ranahiren27');
+
+
+
+console.log('\n balance of ranahire27 is ',ranaCoin.getBalanceOfAddress('ranahiren27'));
